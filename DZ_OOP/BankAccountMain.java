e00d3e3621213047c06232618901c11dc9bc76a8
L1n01ium <znadzafov2007@gmail.com>
2025-11-15 16:25:26 +0300

    Update BankAccountMain.java

--- DZ_OOP/BankAccountMain.java
+++ DZ_OOP/BankAccountMain.java
@@ -1,8 +1,8 @@
 public class BankAccountMain {
 	public static void main(String[] args) {
 		BankAccount bank = new BankAccount();
-		BankAccount bank1 = new BankAccount("Sberbank", "1243");
-		BankAccount bank2 = new BankAccount("Sberbank", "1243", 302000.32, "rub", true);
+		BankAccount bank1 = new BankAccount("1243", "Zaur");
+		BankAccount bank2 = new BankAccount("1243", "Zaur", 302000.32, "rub", true);
 		bank2.displayInfo();
 		bank2.deposit(132003.3);
 		bank2.withdraw(200000.5);
